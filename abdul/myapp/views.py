bdbafc03505a236331f925defbfb5f3d162ec7d4
abduganievabdullokh <210076@akfauniversity.org>
2023-06-03 14:07:21 +0500

    Test

---
+++ abdul/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+from rest_framework import generics
+from .models import Student
+
+# dsda
+# def home(request):
+#     return HttpResponse('Mukhtor')
+#
+# def room(request):
+#     return HttpResponse('This is test room')
